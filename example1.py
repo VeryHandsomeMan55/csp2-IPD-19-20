516d2d321077a9b1460f31273c84577f4eb8372e
VeryHandsomeMan55 <Brodsky0562@gmail.com>
2020-02-19 17:57:30 +0000

    edited elif statement

--- example1.py
+++ example1.py
@@ -10,26 +10,13 @@ team_name = 'E1'
 strategy_name = 'Betray'
 strategy_description = 'Always betray.'
     
-def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    #This example player always betrays.      
-    return 'b'
-
 import random
 
-def Stra_pt1(my_history, their_history, my_score, their_score):
+def move(my_history, their_history, my_score, their_score):
   '''50% chance to put a c and 50% chance to put a b'''
-  choice == random.randint(1, 2)
-  if choice == 1:
+  ch = 0
+  ch == random.randint(1, 2)
+  if ch == 1:
     return 'c'
-  elif choice == 2:
+  elif ch == 2:
     return 'b'

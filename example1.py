7fb393491170b652ace7c0116329edd455853a76
VeryHandsomeMan55 <Brodsky0562@gmail.com>
2020-02-19 17:59:16 +0000

    Our function is inside

--- example1.py
+++ example1.py
@@ -11,15 +11,17 @@ strategy_name = 'Betray'
 strategy_description = 'Always betray.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    #This example player always betrays.      
+  ''' That it will be a function where it will check if it is the first round it will conclude. The last few rounds of games, it will betray, if it is false, then it will see if the other person betrayed last time, if it is false, then it will do a coin flip to see if it will betray or conclude. They all return betray except the coin flip and at the beginning.'''
+  if len(my_history) == 8 or len(my_history) == 9:
     return 'b'
+  elif len(my_history) == 0:
+    return 'b'
+  elif my_history[-1]=='c' and their_history[-1]=='b':
+    return 'b' 
+  else:
+    ch = 0
+    ch == random.randint(1, 2)
+    if ch == 1:
+      return 'c'
+    elif ch == 2:
+      return 'b'
